7951e8429cbd33d59beba0baea4c3497c5d16d4b
viratsaini <viratsaini.vr@gmail.com>
2023-11-01 12:59:51 +0530

    auth.j

--- routes/auth.js
+++ routes/auth.js
@@ -1,24 +1,54 @@
-const express=require("express");
+const express = require("express");
 const User = require("../Models/User");
-const router=express.Router();
-const { body, validationResult } = require('express-validator');
+const router = express.Router();
+const { body, validationResult } = require("express-validator");
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const scrkey = "viratsaini@9917"
 
-router.post('/',[
-    body('name',"Enter a valid name").isLength({min:3}),
-    body('email',"Enter a valid email").notEmpty().isEmail(),
-    body('password',"passoword must be atleast five character").isLength({min :3})
-    ],(req,res)=>{
+//create a user: using "/api/auth/createuser". NO login requir
+router.post(
+  "/createuser",
+  [
+    body("name", "Enter a valid name").isLength({ min: 3 }),
+    body("email", "Enter a valid email").notEmpty().isEmail(),
+    body("password", "passoword must be atleast five character").isLength({min: 3,}),
+  ],
+
+//if there is error return bad request and the errors
+  async (req, res) => {
+    //check that the given userid password and email are correct according to defined rule and give error if not
     const result = validationResult(req);
-  if (!result.isEmpty()) {
-    return res.send({ errors: result.array() });
-  }
-    User.create({
-        name:req.body.name,
-        password:req.body.password,
-        email:req.body.email,
-    }).then(user=>res.json(user))
-    .catch(err=>{console.log(err)
-        res.json({erroe:"This email is already added",message:err.message})})
-})
+    if (!result.isEmpty()) {
+      return res.send({ errors: result.array() });
+    }
+//make salt and add salt with password and hash it and make safe password
+    const salt= await bcrypt.genSalt(10);
+    const safepass=await bcrypt.hash(req.body.password,salt);
 
-module.exports=router
+//check wather the user email is arlady exists
+    try {
+      let user = await User.findOne({ email: req.body.email });
+      if (user) {
+        return res.status(400).json({ Error: "this email is arlady exist" });
+      }
+
+//if email is not arlady exists make the user       
+      user = await User.create({
+        name: req.body.name,
+        password: safepass,
+        email: req.body.email,
+      });
+// make data and token using jwt and pass to user
+      const data ={
+        user:{
+           id: user.id}
+          };
+      const token = jwt.sign(data, scrkey);
+      res.json({token});
+    } 
+    catch (error) { console.error(error.message);
+    res.status  (500).send("some error accurs")}
+  }
+);
+module.exports = router;
