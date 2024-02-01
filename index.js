4d172e7aacf4a9b2d66d694b1466b61dbd0f566b
viratsaini <viratsaini.vr@gmail.com>
2024-02-01 23:51:29 +0530

    add cors

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const connectToMongo = require("./db");
 //connecting the server with express and db
 const express = require('express')
-connectToMongo();
+var cors = require('cors')
 
+connectToMongo();
 const app = express()
 const port = 5000
+
 app.use(express.json());
+app.use(cors())
 
 //avalible routes and defining the api for routes.
 app.use('/api/auth',require('./routes/auth'))
